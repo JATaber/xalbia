95ad6bbf9b09bb4cce7aacb277f166c22b6fa9e1
James Taber <JATaber@fullsail.edu>
2017-07-23 23:42:02 -0500

    work
    working on the designing

--- javascript/script.js
+++ javascript/script.js
@@ -1,87 +1,68 @@
-/**
- * Created by jamestaber on 7/21/17.
- */
-const subForm = document.querySelector('.searchForm');
-const searchInput = document.querySelector('#input');
+var request = new XMLHttpRequest();
+
+var url = 'https://api.twitch.tv/kraken/search/streams?client_id=81twkqgzahbim1nmtd47klviib0vck&query=league&limit=12';
+
+request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(request.responseText);
+
+        console.log(data);
+
+        var ele = document.getElementById("searchResults");
 
 
-//listen for forms submission
-function searchForm(event) {
+        if (ele) {
+            var gameInfo = document.querySelector("#searchResults");
 
-    //stop default submit action
-    event.preventDefault();
+            console.log(data.streams.length);
 
-    //take in value from form
-    const query = searchInput.value;
+            var searchData= '';
 
-    console.log(query);
+            if(data.streams.length > 0){
 
-    //set additional values
-    const limit = "12";
-    const client_Id = "81twkqgzahbim1nmtd47klviib0vck";
-
-    var request = new XMLHttpRequest();
-
-    //build query string
-    const api = 'https://api.twitch.tv/kraken/search/streams?client_id=' + client_Id + '&query=' + query + '&limit=' + limit;
-    console.log(api);
-
-    request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
-
-            console.log(data);
-
-            var ele = document.getElementById("results");
-
-            if (ele) {
-                var gameInfo = document.querySelector("#searchResults");
-                var title = document.querySelector('#resultsInfo');
-
-                console.log(data.streams.length);
-
-                var searchData='';
-
-                gameInfo.innerHTML='';
-
-                title.innerHTML= 'results for '+query;
-
-                if(data.streams.length >0){
-
-                    document.querySelector("#userFeedback").innerHTML = '';
-
-                    for (var i = 0; i < data.streams.length; i++)  {
-                        searchData += '<article>';
-                        searchData += '<a href="' + data.streams[i].channel.url + '" target="_blank">';
-                        searchData += '<img src=' + data.streams[i].preview.large + ' alt="preview">';
-                        searchData += '<h3>' + data.streams[i].channel.name + '<strong>' + data.streams[i].channel.game + '</strong></h3>';
-                        searchData += '</a>';
-                        searchData += '</article>';
-                    }
-                }else{
-                    document.querySelector("#userFeedback").innerHTML = "Your search results didn't return anything";
+                for (var i = 0; i < data.streams.length; i++)  {
+                    searchData += '<article>';
+                    searchData += '<a href="' + data.streams[i].channel.url + '" target="_blank">';
+                    searchData += '<img src=' + data.streams[i].preview.large + ' alt="preview">';
+                    searchData += '<h3>' + data.streams[i].channel.name + '<strong>' + data.streams[i].channel.game + '</strong></h3>';
+                    searchData += '</a>';
+                    searchData += '</article>';
                 }
+            }else{
+                document.querySelector("#userFeedback").innerHTML = "Your search results didn't return anything";
 
-                gameInfo.insertAdjacentHTML('beforeEnd', searchData);
-                console.log(data.streams[0]);
-
-            } else {
-                console.log('response error');
             }
 
-            request.onerror = function () {
 
-                console.log('connection error');
-            };
-            //request close
+            gameInfo.insertAdjacentHTML('beforeEnd', searchData);
+
+
+            console.log(data.streams[0]);
+
+        } else {
+            console.log('response error')
         }
+
+        request.onerror = function () {
+
+            console.log('connection error');
+        };
+        //request close
+    }
 //onload close
-    };
+};
 
-    request.open('GET', api, true);
-    request.send();
+request.open('GET', url, true);
+request.send();
 
-    //search function end
+
+function show(){
+
+    var x = document.getElementById('nav');
+    if(x.style.display === 'none'){
+        x.style.display = 'block';
+    }else{
+        x.style.display = 'none';
+    }
+
 }
-
-subForm.addEventListener('submit', searchForm, false);
