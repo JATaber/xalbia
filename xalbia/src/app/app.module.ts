b48822b73eb750f1deedb7e279aba1c00b5fc521
James Adam Taber <taberdev@gmail.com>
2021-01-20 10:34:01 -0600

    base version of the angular application

--- xalbia/src/app/app.module.ts
+++ xalbia/src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MainComponent
+    NavbarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
